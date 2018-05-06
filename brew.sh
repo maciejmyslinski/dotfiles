d6b200feb669d5b6b921f97b08e0807bf97fa082
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-05-07 01:56:27 +0200

    Do not install homebrew

--- brew.sh
+++ brew.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
